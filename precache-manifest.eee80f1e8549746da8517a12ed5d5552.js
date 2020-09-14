7b8fc195f9c8b2c6d0f098c50648e5230d8d6184
Kabita Rawal <kabitarawl88@gmail.com>
2020-09-15 00:46:19 +0545

    Updates

---
+++ precache-manifest.eee80f1e8549746da8517a12ed5d5552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72fef26246a1b925cb9e6cc5bbeceb9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3e05868969650230b7e5",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d48cd86c5aa4d09124cb",
+    "url": "/portfolio/static/css/main.17322a70.chunk.css"
+  },
+  {
+    "revision": "3e05868969650230b7e5",
+    "url": "/portfolio/static/js/2.37256c0d.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/portfolio/static/js/2.37256c0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48cd86c5aa4d09124cb",
+    "url": "/portfolio/static/js/main.3e8efd72.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
